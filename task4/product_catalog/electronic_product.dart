9b2c0cc4da7fc2841ab06a72706886e11d90182d
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-14 06:20:41 +0300

    create ElectronicProduct class

---
+++ task4/product_catalog/electronic_product.dart
@@ -0,0 +1,20 @@
+import 'product.dart';
+
+class ElectronicProduct extends Product {
+  int _warrantyPeriod; // in months
+
+  ElectronicProduct({
+    required String id,
+    required String name,
+    required double price,
+    required String description,
+    required int warrantyPeriod,
+  }) : _warrantyPeriod = warrantyPeriod,
+       super(id: id, name: name, price: price, description: description);
+  int get warrantyPeriod => _warrantyPeriod;
+  set warrantyPeriod(int warrantyPeriod) => _warrantyPeriod = warrantyPeriod;
+
+  String formatProductDetails() {
+    return 'ElectronicProduct{id: $id, name: $name, price: $price, description: $description, warrantyPeriod: $_warrantyPeriod months}';
+  }
+}
