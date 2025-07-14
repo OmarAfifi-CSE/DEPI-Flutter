196b7e2d18265a0230565238f16fff04a63b7606
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-14 08:57:07 +0300

    update inventory

--- task4/inventory_management/inventory.dart
+++ task4/inventory_management/inventory.dart
@@ -3,39 +3,61 @@ import '../user_management/admin.dart';
 
 class Inventory {
   final Admin _admin;
-  final Map<Product, int> _stock = {};
+  List<Map<Product, int>> _stock = [];
 
   Inventory({required Admin admin}) : _admin = admin;
   Admin get admin => _admin;
-  Map<Product, int> get stock => _stock;
+  List<Map<Product, int>> get stock => _stock;
+  set stock(List<Map<Product, int>> newStock) {
+    _stock = newStock;
+  }
 
   void addProduct(Product product, int quantity) {
     if (quantity <= 0) {
       print('Quantity must be greater than zero.');
       return;
     }
-    _stock.update(
-      product,
-      (currentQuantity) => currentQuantity + quantity,
-      ifAbsent: () => quantity,
-    );
-    print("$quantity unit of '$product' added. New total: ${_stock[product]}.");
+
+    // Check if the product exists
+    final index = _stock.indexWhere((item) => item.containsKey(product));
+    if (index != -1) {
+      _stock[index].update(
+        product,
+        (existingQuantity) => existingQuantity + quantity,
+        ifAbsent: () => quantity,
+      );
+      print(
+        "$quantity unit of '$product' added. New total: ${_stock[index][product]}.",
+      );
+    } else {
+      _stock.add({product: quantity});
+      print("$quantity unit of '$product' added to stock.");
+    }
   }
 
   bool stockAvailableCheck(Product product, int quantity) {
-    final availableQuantity = _stock[product] ?? 0;
+    final itemMap = _stock.firstWhere(
+      (item) => item.containsKey(product),
+      orElse: () => {},
+    );
+    if (itemMap.isEmpty) {
+      return false;
+    }
+
+    final availableQuantity = itemMap[product] ?? 0;
     return availableQuantity >= quantity;
   }
 
   void updateStock(Product product, int quantity) {
     if (!stockAvailableCheck(product, quantity)) {
       throw Exception(
-        'Failed to update stock: Not enough quantity for $product. Available: ${_stock[product] ?? 0}, Required: $quantity',
+        'Failed to update stock: Not enough quantity for $product. Required: $quantity',
       );
     }
-    _stock[product] = _stock[product]! - quantity;
+    final index = _stock.indexWhere((item) => item.containsKey(product));
+    _stock[index][product] = _stock[index][product]! - quantity;
     print(
-      "Stock updated successfully, New product quantity: ${_stock[product]}.",
+      "Stock updated successfully, New product quantity: ${_stock[index][product]}.",
     );
   }
 }
