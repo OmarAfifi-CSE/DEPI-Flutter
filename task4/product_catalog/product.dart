07ac0c3c35562921d7003fd05242a71205f47792
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-14 06:20:23 +0300

    Create product class

---
+++ task4/product_catalog/product.dart
@@ -0,0 +1,28 @@
+abstract class Product{
+  final String _id;
+  String _name;
+  double _price;
+  String _description;
+  String formatProductDetails();
+
+  Product({
+    required String id,
+    required String name,
+    required double price,
+    required String description,
+  }): _id = id,
+        _name = name,
+        _price = price,
+        _description = description;
+  String get id => _id;
+  String get name => _name;
+  double get price => _price;
+  String get description => _description;
+  set name(String name) => _name = name;
+  set price(double price) => _price = price;
+  set description(String description) => _description = description;
+  @override
+  String toString() {
+    return 'Product{id: $_id, name: $_name, price: $_price, description: $_description}';
+  }
+}
