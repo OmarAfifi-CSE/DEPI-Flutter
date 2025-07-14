f9a02d5e118cba122518b872151319588296e6e3
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-14 05:41:54 +0300

    refactor user class

--- task4/user.dart
+++ task4/user.dart
@@ -1,16 +1,20 @@
 
 class User{
-  String? _id;
-  String? _email;
-  String? _password;
+  String _id;
+  String _email;
+  String _password;
 
-  User(this._id, this._email, this._password);
-  String? get id => _id;
-  String? get email => _email;
-  String? get password => _password;
-  set id(String? id) => _id = id;
-  set email(String? email) => _email = email;
-  set password(String? password) => _password = password;
+  User({required String id, required String email, required String password}) :
+        _id = id,
+        _email = email,
+        _password = password;
+
+  String get id => _id;
+  String get email => _email;
+  String get password => _password;
+  set id(String id) => _id = id;
+  set email(String email) => _email = email;
+  set password(String password) => _password = password;
 
   @override
   String toString() {
