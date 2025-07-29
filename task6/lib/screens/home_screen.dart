8558f731978a51b4081742485cb3828a341a5178
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-29 17:38:15 +0300

    create search bar

--- task6/lib/screens/home_screen.dart
+++ task6/lib/screens/home_screen.dart
@@ -19,6 +19,31 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: 'Search for food',
+                  hintStyle: const TextStyle(
+                    color: Color(0xffB89494),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xffF3E7E7),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
