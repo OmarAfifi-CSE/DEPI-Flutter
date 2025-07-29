5b0e8bb0a761c05be4bc0f250f0b5b035aa061de
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-29 20:54:17 +0300

    create food list view

---
+++ task6/lib/widgets/food_list_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:task6/models/food_item.dart';
+
+import 'add_to_cart_button.dart';
+
+class FoodListView extends StatelessWidget {
+  final List<FoodItem> _items;
+
+  const FoodListView({super.key, required List<FoodItem> items})
+    : _items = items;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: _items.length,
+      itemBuilder: (context, index) {
+        final item = _items[index];
+        return ListTile(
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 0,
+            horizontal: 0,
+          ),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(item.imagePath, width: 80, fit: BoxFit.cover),
+          ),
+          title: Text(item.title),
+          subtitle: Text(item.subtitle),
+          trailing: AddToCartButton(),
+        );
+      },
+    );
+  }
+}
