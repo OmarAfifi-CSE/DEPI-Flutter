5f5a2d23dae34ecb658f74828721c50d99a89c50
Omar Afifi <omarafifi.cse@gmail.com>
2025-08-16 22:43:31 +0300

    create movie model

--- task8/lib/helpers/app_assets.dart
+++ task8/lib/helpers/app_assets.dart
@@ -1,4 +1,4 @@
 class AppAssets {
-  static const movie1 = "assets/images/Movie-1.png";
-  static const movie2 = "assets/images/Movie-2.png";
+  static const String movie1 = 'assets/images/Movie-1.png';
+  static const String movie2 = 'assets/images/Movie-2.png';
 }
