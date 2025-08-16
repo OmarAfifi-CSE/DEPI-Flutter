5a767b9be09c6dfdc3aba5f3038e6b991d5cd786
Omar Afifi <omarafifi.cse@gmail.com>
2025-08-16 12:12:27 +0300

    create app colors

---
+++ task8/lib/helpers/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const backgroundColor = Color(0xFF171212);
+  static const primaryColor = Color(0xFF382929);
+  static const secondaryColor = Color(0xFFB89E9E);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const yellowColor = Color(0xFFF7CB1A);
+}
