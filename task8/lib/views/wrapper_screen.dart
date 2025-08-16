36ca4892f6d0d12d453203793e7e1829973faaf9
Omar Afifi <omarafifi.cse@gmail.com>
2025-08-16 22:45:24 +0300

    create wrapper screen

--- task8/lib/views/wrapper_screen.dart
+++ task8/lib/views/wrapper_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task8/views/watchlist_screen.dart';
 
 import '../helpers/app_colors.dart';
 import 'home_screen.dart';
@@ -13,20 +14,34 @@ class WrapperScreen extends StatefulWidget {
 class _WrapperScreenState extends State<WrapperScreen> {
   final PageController _pageController = PageController();
   int _currentScreen = 0;
-  final List<Widget> _screens = [const HomeScreen()];
+  final List<Widget> _screens = [const HomeScreen(), const WatchlistScreen()];
   final List<String> _titles = ['Movie Catalog', 'Watchlist'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
         title: Text(_titles[_currentScreen]),
+        titleTextStyle: const TextStyle(
+          color: AppColors.whiteColor,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
         centerTitle: true,
         actions: [
-          _currentScreen == 0
-              ? IconButton(icon: const Icon(Icons.search), onPressed: () {})
+          _currentScreen == 1
+              ? IconButton(
+                  icon: const Icon(Icons.search),
+                  color: AppColors.whiteColor,
+                  iconSize: 24,
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  onPressed: () {},
+                )
               : Container(),
         ],
+        backgroundColor: AppColors.backgroundColor,
+        elevation: 0,
       ),
       body: PageView(
         controller: _pageController,
@@ -47,7 +62,6 @@ class _WrapperScreenState extends State<WrapperScreen> {
         selectedFontSize: 12,
         unselectedFontSize: 12,
         iconSize: 24,
-
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
@@ -64,6 +78,7 @@ class _WrapperScreenState extends State<WrapperScreen> {
         onTap: (index) {
           setState(() {
             _currentScreen = index;
+            _pageController.jumpToPage(index);
           });
         },
       ),
