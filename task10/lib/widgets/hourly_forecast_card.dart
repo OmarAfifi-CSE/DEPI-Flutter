cf73332fcbd98b06a1ff380fe50a9b1918d51a75
Omar Afifi <omarafifi.cse@gmail.com>
2025-08-27 01:36:48 +0300

    create hourly forecast card

---
+++ task10/lib/widgets/hourly_forecast_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HourlyForecastCard extends StatelessWidget {
+  final String time;
+  final String iconUrl;
+  final String temperature;
+
+  const HourlyForecastCard({
+    super.key,
+    required this.time,
+    required this.iconUrl,
+    required this.temperature,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80.w,
+      padding: EdgeInsets.symmetric(vertical: 10.h),
+      decoration: BoxDecoration(
+        color: Colors.white.withValues(alpha: 0.15),
+        borderRadius: BorderRadius.circular(20.r),
+      ),
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Text(
+            time,
+            style: TextStyle(color: Colors.white, fontSize: 16.sp),
+          ),
+          Positioned(
+            bottom: 0,
+            top: 0,
+            child: Image.network(
+              'https:$iconUrl',
+              height: 50.h,
+              width: 50.w,
+              errorBuilder: (context, error, stackTrace) =>
+                  const Icon(Icons.cloud_off, color: Colors.white, size: 40),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Text(
+              temperature,
+              style: TextStyle(color: Colors.white, fontSize: 16.sp),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
