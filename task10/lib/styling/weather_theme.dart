3e44295cb7a9b7bdfa2656dc08f8108095faeb20
Omar Afifi <omarafifi.cse@gmail.com>
2025-08-29 06:15:01 +0300

    fix weather codes

--- task10/lib/styling/weather_theme.dart
+++ task10/lib/styling/weather_theme.dart
@@ -63,34 +63,39 @@ class WeatherTheme {
       case 1003:
       case 1006:
       case 1009:
+      case 1030:
+      case 1135:
+      case 1147:
         return cloudyTheme;
-      case 1050:
-      case 1053:
       case 1063:
+      case 1069:
+      case 1072:
+      case 1087:
+      case 1150:
+      case 1153:
+      case 1168:
+      case 1171:
       case 1180:
       case 1183:
+      case 1186:
       case 1189:
       case 1192:
       case 1195:
       case 1198:
       case 1201:
-      case 1207:
       case 1204:
+      case 1207:
       case 1240:
       case 1243:
       case 1246:
+      case 1249:
+      case 1252:
       case 1273:
       case 1276:
         return rainyTheme;
       case 1066:
-      case 1068:
-      case 1069:
-      case 1071:
-      case 1072:
       case 1114:
       case 1117:
-      case 1135:
-      case 1147:
       case 1210:
       case 1213:
       case 1216:
@@ -98,8 +103,6 @@ class WeatherTheme {
       case 1222:
       case 1225:
       case 1237:
-      case 1249:
-      case 1252:
       case 1255:
       case 1258:
       case 1261:
